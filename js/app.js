1fee49963fb9316c36d23cb0e28a0c51e91d6627
ctrlclos <carloseduardoalvesdemoura9@gmail.com>
2025-11-16 19:25:01 -0800

    Initial project setup with file structure.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//Verify JavaScript is loading
+console.log('app.js loaded');
+//Constants
+
+//State variables
+
+//Cached elements references
+
+//Event listeners
+
+//Functions
+// init() initializes the game.
+function init() {
+  console.log("Checker's game initialized");
+}
+
+// Call init when the dom is ready
+document.addEventListener('DOMContentLoaded', init);
